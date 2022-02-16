db45b57e161f8b235121e522e159f03b0ae603f1
Sneha <snhthakare@gmail.com>
2022-02-16 15:02:36 +0530

    Adds database connection

--- server/index.js
+++ server/index.js
@@ -2,3 +2,23 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+
+const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json({ extended: true }))
+
+app.use(cors)
+
+// Replace the following with your Atlas connection string                                                                                                                                        
+const CONNECTION_URL = "mongodb+srv://snhthakare:sneha%40123@cluster0.ui0ub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running successfully on post ${PORT}`)))
+    .catch((err) => { console.log(err) })
+
+
